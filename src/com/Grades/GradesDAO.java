1129ff20a6077ceb5b886aca2a6aec6f87f8d37e
Jack Caltagirone <35774493+JackCaltagirone@users.noreply.github.com>
2019-03-28 10:10:04 -0700

    quick touch up i forgot in the last one

--- src/com/Grades/GradesDAO.java
+++ src/com/Grades/GradesDAO.java
@@ -30,19 +30,25 @@ public class GradesDAO {
 		Connection conn = mysqlDS.getConnection();
 		Statement myStmt = conn.createStatement();
 
-		String query = "select * from Grades";
+		String query = "select * from grades";
 		ResultSet rs = myStmt.executeQuery(query);
 
 		ArrayList<Grades> Grade = new ArrayList<Grades>();
 
 		while (rs.next()) {
-			String sid = rs.getString("sid");
-			String name = rs.getString("name");
-			String address = rs.getString("Address");
-			String dob = rs.getString("Date_of_Birth");
-			String yearOrClass = rs.getString("year_or_class");
-			String special_Needs = rs.getString("Special_Needs");
-			Grades g = new Grades(sid, dob, name, address, yearOrClass, special_Needs);
+			
+			
+			System.out.println("before");
+			int math = rs.getInt("math");  //breaking line. "Column math not found"
+			System.out.println("after");
+			
+			
+			int english = rs.getInt("english");
+			int irish = rs.getInt("irish");
+			int business = rs.getInt("business");
+			int science = rs.getInt("science");
+			int pe = rs.getInt("pe");
+			Grades g = new Grades(math, english, irish, business, science, pe);
 
 			Grade.add(g);
 		}
@@ -51,25 +57,25 @@ public class GradesDAO {
 
 	}
 
-	public void insertGrade(Grades s) throws SQLException {
+	public void insertGrade(Grades g) throws SQLException {
 
-		String sid = s.getSid();
-		String name = s.getName();
-		String address = s.getAddress();
-		String date_of_Birth = s.getDate_of_Birth();
-		String year_or_class = s.getYear_or_class();
-		String Special_Needs = s.getSpecial_Needs();
+		int math = g.getMath();
+		int english = g.getEnglish();
+		int irish = g.getIrish();
+		int business = g.getBusiness();
+		int science = g.getScience();
+		int pe = g.getPe();
 
 		Connection conn = mysqlDS.getConnection();
 		PreparedStatement myStat = conn.prepareStatement(
-				"INSERT INTO Grades (sid, name, address, date_of_Birth,year_or_class,Special_Needs) VALUES (?, ?, ?, ?,?,?)");
+				"INSERT INTO Grades (math, english, irish, business, science,pe) VALUES (?, ?, ?, ?,?,?)");
 
-		myStat.setString(1, sid);
-		myStat.setString(2, name);
-		myStat.setString(3, address);
-		myStat.setString(4, date_of_Birth);
-		myStat.setString(5, year_or_class);
-		myStat.setString(6, Special_Needs);
+		myStat.setInt(1, math);
+		myStat.setInt(2, english);
+		myStat.setInt(3, irish);
+		myStat.setInt(4, business);
+		myStat.setInt(5, science);
+		myStat.setInt(6, pe);
 
 		myStat.executeUpdate();
 
